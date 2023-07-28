16d79d64da975a385764ece4c2f878a5e7b9baf3
Soma-0104 <somasundarir612@gmail.com>
2023-07-28 11:46:45 +0530

    Revert "FrontEnd"
    This reverts commit 0cdc8648f643d92e0ceafaeace3573014131e6bb.

--- Front end/src/Components/feedbackApi.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-const URI='http://localhost:8081'
-const getFeedback = () => axios.get(`${URI}/getfed`)
-const addFeedback = (Feedback) => axios.post(`${URI}/addfed`,Feedback)
-export {getFeedback,addFeedback}
