0bfd444be212d4ba2829477b13462094308f77f5
Soma-0104 <somasundarir612@gmail.com>
2023-08-01 09:51:43 +0530

    CarSalesSystem_ConsoleApplication

---
+++ Mini Project/src/com/example/database/Database.java
@@ -0,0 +1,23 @@
+package com.example.database;
+import java.sql.*;
+public class Database {
+	static Connection con;
+	public static Connection createc() 
+		{	
+			try 
+			{
+				
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				
+				String user = "root"; 
+				String password = "Soma@152"; 
+				String url = "jdbc:mysql://localhost:3306/car";
+				con = DriverManager.getConnection(url,user,password);
+			}
+			catch(Exception e) 
+			{
+				e.printStackTrace();
+			}
+			return con;
+		}
+}
